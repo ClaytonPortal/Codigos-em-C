17209d62a969c6f832625f5abebcbdd5e0a2f86f
ClaytonPortal <130517321+ClaytonPortal@users.noreply.github.com>
2023-04-11 23:50:58 -0300

    Update laboratorio1.c

--- laboratorio1.c
+++ laboratorio1.c
@@ -3,9 +3,11 @@
 
 #define TAM 10000
 
+// declarando vetores
 long vetor[TAM];
 long copiaVetor[TAM];
 
+// funcao das threads
 void *multiVetor(void *id) {
     long threadID = *(long *) id;
     
@@ -24,31 +26,37 @@ void *multiVetor(void *id) {
 }
 
 int main(void) {
+    
+    // inicializando os vetores (original e copia)
     for (long i = 0; i < TAM; i++){
         vetor[i] = i+1;
         copiaVetor[i] = vetor[i];
     }
     
+    // declarando threads e ids
     pthread_t T[2];
     long idthreads[2];
-
+    
+    // criando as threads
     for(long i = 0; i < 2; i++){
         idthreads[i] = i;
         pthread_create(&T[i], NULL, multiVetor, (void *) &idthreads[i]);
     }
 
+    // join das threads
     for(long i = 0; i < 2; i++){
         pthread_join(T[i], NULL);
     }
     
+    // testando se o vetor foi corretamente multiplicado
     for (long i = 0; i < TAM; i++){
         if (copiaVetor[i] != (vetor[i]/2)){
-            printf("ERRO");
+            printf("ERRO!"); // mensagem de erro
             return 1;
         }
     }
     
-    printf("Vetor funcionando!");
+    printf("Vetor funcionando!"); // mensagem funcionando
     pthread_exit(NULL);
     
     return 0;
